335e394b81d5d7c94aac99705de212220fb469a5
Erik van Dam <erikvandam93@gmail.com>
2025-01-26 19:31:42 +0100

    feat: added design system package

--- packages/tailwind-config/config.ts
+++ packages/tailwind-config/config.ts
@@ -2,7 +2,12 @@ import type { Config } from 'tailwindcss'
 import animate from 'tailwindcss-animate'
 
 export const config: Config = {
+  content: [
+    './node_modules/@internal/design-system/components/**/*.{ts,tsx}',
+    './node_modules/@internal/design-system/hooks/**/*.{ts,tsx}',
+    './node_modules/@internal/design-system/index.tsx',
+    './src/**/*.{ts,tsx}',
+  ],
   darkMode: ['class'],
-  content: ['./src/**/*.{ts,tsx}'],
   plugins: [animate],
 }
